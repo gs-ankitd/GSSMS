611c7c0e65fbaf193ad227aa0055c92865bde8b0
Ankit Dedhia <ankitd@gsteckno.com>
2016-11-15 14:41:10 +0530

    cHANGE tITLE
    Signed-off-by: Ankit Dedhia <ankitd@gsteckno.com>

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,7 +3,7 @@
   
 <head> 
     <title>
-      SMS Application
+      SMS App
     </title>
     <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700" media="all" rel="stylesheet" type="text/css" />
 	<!--<link href="<?php echo base_url(); ?>stylesheets/bootstrap.min.css" media="all" rel="stylesheet" type="text/css" />-->
